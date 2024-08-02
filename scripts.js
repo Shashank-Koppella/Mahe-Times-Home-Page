ca8b33002f19a75af87a0d907a45c60d7b4661db
Shashank-Koppella <100149433+Shashank-Koppella@users.noreply.github.com>
2024-08-02 19:49:16 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,9 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
-  // Select the Connections button
   const connectionsButton = document.querySelector('.connections-button');
-
-  // Add a click event listener to redirect on click
   connectionsButton.addEventListener('click', () => {
-      window.location.href = 'https://shashank-koppella.github.io/MAHEConnections/connections.html';
+    window.location.href = 'https://shashank-koppella.github.io/MAHEConnections/connections.html';
   });
 });
